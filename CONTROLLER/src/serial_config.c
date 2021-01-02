5b5893d04d39dddc8ba0cd9e495299fcedcecf4d
franeum <elettrochuck@gmail.com>
2021-01-02 01:20:57 +0100

    fix serial format

--- CONTROLLER/src/serial_config.c
+++ CONTROLLER/src/serial_config.c
@@ -12,12 +12,12 @@
 
 void run_serial_config(int number) {
     uart_config_t uart_config = {
-        .baud_rate = ECHO_UART_BAUD_RATE,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_APB,
+        .baud_rate      = ECHO_UART_BAUD_RATE,
+        .data_bits      = UART_DATA_8_BITS,
+        .parity         = UART_PARITY_DISABLE,
+        .stop_bits      = UART_STOP_BITS_1,
+        .flow_ctrl      = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk     = UART_SCLK_APB,
     };
 
     int intr_alloc_flags = 0;
@@ -26,7 +26,6 @@ void run_serial_config(int number) {
     #if CONFIG_UART_ISR_IN_IRAM
         intr_alloc_flags = ESP_INTR_FLAG_IRAM;
     #endif
-
 
     ESP_ERROR_CHECK(uart_driver_install(number, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
     ESP_ERROR_CHECK(uart_param_config(number, &uart_config));
